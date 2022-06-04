22cbcd7cadaeb11cb580b8dff3535b8cbbd69321
blacklytning <blacklytning@yandex.com>
2022-06-04 11:38:31 +0530

    hanoip: Adapt for OrangeFox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,65 @@
-export ALLOW_MISSING_DEPENDENCIES=true
-export LC_ALL=C
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="hanoip"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+        export OF_AB_DEVICE=1
+	export LC_ALL="C"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+ 	
+        # export TARGET_DEVICE_ALT="generic"
+	# export OF_TARGET_DEVICES="generic"
+	
+	# Screen Settings
+	export OF_SCREEN_H=2460 
+	export OF_STATUS_H=116 
+	export OF_STATUS_INDENT_LEFT=35
+	export OF_STATUS_INDENT_RIGHT=35 
+	export OF_HIDE_NOTCH=1 
+	export OF_CLOCK_POS=1
+	
+	# R11.1 Settings
+	export FOX_VERSION="R11.1"
+	export OF_MAINTAINER="blacklytning"
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
+
